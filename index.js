76b2cb02d9854a847af68d0cbe569c122e6a9c7a
nyabangWech <nyabangwech@gmail.com>
2024-05-11 14:23:57 +0300

    added changes

--- index.js
+++ index.js
@@ -6,8 +6,7 @@ function message(introduce,name){
 }
 setTimeout(message,3000,"hello there","i am learning");
 
-//an array of IDs and afunction  getUseData(id) that  returns a promise user data
-//when given a user ID.
+
 
 function getUseData(userId,callback){
 setTimeout(() => {
@@ -25,9 +24,7 @@ function getuserData(userId){
     })
 }
 
-// an asynchronous function performTask() that returns a Promise. The Promise resolves if the task is successful and rejects if
-//there's an error. Write a function that calls performTask() and logs a custom success message if the task is successful, and a custom
-// error message if there's an error.
+
 const result = true;
 const ourPromise = new Promise((resolve, reject)=>{
     if(result){
@@ -57,15 +54,6 @@ async function performTask(){
 }
 performTask();
 
-// Write a function unstableTask that:
-// 1.Accepts a taskName and failureProbability (a number between 0 and 1).
-// 2. Returns a Promise that:
-// Resolves immediately with a success message if a randomly generated number is greater than failureProbability.
-// Rejects immediately with a failure message if a randomly generated number is less than or equal to failureProbability.
-// Write another function executeWithRetry that:
-// Accepts a taskName, retries, and failureProbability.
-// Uses a retry mechanism to attempt the unstableTask up to retries times.
-// Logs whether the task succeeded or failed after all attempts.
 async function executeWithRetry(taskName, retries, failureProbability) {
     let attempt = 0;
     while (attempt < retries) {
